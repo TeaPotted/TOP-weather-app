3c1f87504ed693afab402f0031735332db16f8f1
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-05-28 23:23:14 +0800

    create a function that creates a div for a day's date

--- src/create-elements.js
+++ src/create-elements.js
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 // function for creating an element
 function createElem(elem, cls, textContent) {
   const el = document.createElement(elem);
@@ -7,4 +9,14 @@ function createElem(elem, cls, textContent) {
   return el;
 }
 
-export { createElem };
+// function that creates a div that holds elements for the day's date and date
+function createDateDiv(date) {
+  // div to keep the day and date p elements
+  const div = createElem("div", "dateDiv", "");
+  const day = createElem("p", "day", format(date.split("-"), "EEEE")); // day's textContent will be full day name eg: "Monday"
+  const dateP = createElem("p", "date", format(date.split("-"), "d MMM")); // dateP's textContent will be [day Month] eg: "6 Jan"
+  div.append(day, dateP);
+  return div;
+}
+
+export { createElem, createDateDiv };
