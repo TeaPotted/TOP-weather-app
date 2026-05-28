0e9b4effc8ace53a32faa44a03a463a2b64c1193
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-05-28 18:29:24 +0800

    create a function for getting the town data

---
+++ src/get-scripts.js
@@ -0,0 +1,34 @@
+const town = document.getElementById("town"); // get the input#town element
+
+// function for fetching town data
+function getTownData() {
+  // fetch town data
+  return fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${town.value}?unitGroup=metric&include=days&key=P49UUD9RB3YB5FDWBCBR2GAH9`,
+  )
+    .then((response) => {
+      //check the http response code and if it isn't ok then throw the response as an error
+      if (!response.ok) {
+        throw response;
+      }
+
+      return response.json(); //parse the result as JSON
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((errorResponse) => {
+      if (errorResponse.text) {
+        //additional error information
+        errorResponse.text().then((errorMessage) => {
+          //errorMessage now returns the response body which includes the full error message
+          console.log(errorMessage);
+        });
+      } else {
+        //no additional error information
+        console.log(errorResponse);
+      }
+    });
+}
+
+export { getTownData };
