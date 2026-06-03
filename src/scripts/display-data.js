80ccd384219a90f89b5d5535824c966e5e08752d
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-04 02:45:22 +0800

    create a function for displaying the full weather information of a day and make dayDiv call it when clicked

--- src/scripts/display-data.js
+++ src/scripts/display-data.js
@@ -1,4 +1,5 @@
 import { getTownData } from "./get-scripts.js";
+import { format } from "date-fns";
 import {
   createElem,
   createDateDiv,
@@ -6,9 +7,60 @@ import {
   createTemperatureDiv,
   createMaxTempDiv,
   createMinTempDiv,
+  createSunriseDiv,
+  createSunsetDiv,
 } from "./create-elements.js";
 
 const displayWeatherDiv = document.querySelector("div.display-weather");
+
+// function for displaying the full weather information of a day
+function displayFullDay(day) {
+  // if there is already a full-day div, remove it
+  if (document.body.querySelector("div.full-day-div")) {
+    document.body.removeChild(document.querySelector("div.full-day-div"));
+  }
+
+  // create a div to keep all the info in
+  const dayDiv = createElem("div", "full-day-div", "");
+  const dateDiv = createDateDiv(day.datetime);
+  // make .date's (in dateDiv) textContent to display the day of the month and full name of the month
+  dateDiv.querySelector("p.date").textContent = format(
+    day.datetime.split("-"),
+    "d MMMM",
+  );
+
+  // div to keep the temperature div and icon image
+  const tempAndIconDiv = createElem("div", "temp-and-icon", "");
+  const iconImg = createIconImg(day.icon);
+  iconImg.then((response) => {
+    // append both temperatureDiv and the response to tempAndIconDiv
+    tempAndIconDiv.append(createTemperatureDiv(day.temp), response);
+  });
+
+  // div to keep the min temp div , max temp div, sunrise div and sunset div
+  const tempsAndSunTimesDiv = createElem("div", "temperatures-and-suntimes");
+
+  // div to keep the min temp and max temp divs
+  const temperaturesDiv = createElem("div", "temperatures", "");
+  temperaturesDiv.append(
+    createMaxTempDiv(day.tempmax),
+    createMinTempDiv(day.tempmin),
+  );
+
+  // div to keep the sunrise and sunset divs
+  const sunTimesDiv = createElem("div", "sun-times", "");
+  sunTimesDiv.append(
+    createSunriseDiv(day.sunrise),
+    createSunsetDiv(day.sunset),
+  );
+
+  tempsAndSunTimesDiv.append(temperaturesDiv, sunTimesDiv);
+
+  const description = createElem("p", "description", day.description); // the day's description
+
+  dayDiv.append(dateDiv, tempAndIconDiv, tempsAndSunTimesDiv, description);
+  document.body.append(dayDiv);
+}
 
 // function for displaying the information of a day
 function displayDay(day) {
@@ -42,6 +94,7 @@ function displayDay(day) {
         break;
     }
   }
+  dayDiv.addEventListener("click", () => displayFullDay(day));
   return dayDiv;
 }
 
@@ -51,7 +104,7 @@ function displayDays() {
   if (displayWeatherDiv.textContent !== "") {
     displayWeatherDiv.textContent = "";
   }
-  
+
   const days = getTownData();
   days
     .then((response) => {
