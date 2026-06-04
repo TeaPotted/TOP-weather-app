19b6ac328dfee7ce61cdbacef56171d7fe1cec25
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-05 03:35:50 +0800

    when #searc-btn is clicked, call displayDays

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import { checkInput } from "./scripts/check.js";
+import {displayDays} from "./scripts/display-data.js"
 
 const form = document.querySelector("form");
 const input = document.querySelector("input#location");
+const searchBtn = document.getElementById("search-btn");
 
 // when form is submitted, call checkInput
 form.addEventListener("submit", (e) => {
@@ -12,3 +14,10 @@ form.addEventListener("submit", (e) => {
 
 // call checkInput when input has an input
 input.addEventListener("input", () => checkInput());
+
+searchBtn.addEventListener("click", () => {
+  // if checkInput is true, call displayDays
+  if (checkInput()) {
+    displayDays("metric"); // display days in metric system for now
+  }
+})
