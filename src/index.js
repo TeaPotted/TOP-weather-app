0e679fb2c409319917cdf714944df38dcfb76318
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-05 03:25:54 +0800

    call checkInput() when form is submitted and when #location has an input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import "./styles.css"
+import "./styles.css";
+import { checkInput } from "./scripts/check.js";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input#location");
+
+// when form is submitted, call checkInput
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInput();
+});
+
+// call checkInput when input has an input
+input.addEventListener("input", () => checkInput());
